05bbf672b185d68e3dc017263e215e5ced629c41
ARj-ai <131518958+ARj-ai@users.noreply.github.com>
2023-06-08 13:59:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 Jojo = "";
 Harry = "";
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
 
 function preload(){
     Jojo_theme = loadSound("Jojo's theme.mp3");
@@ -11,8 +16,30 @@ canvas =  createCanvas(600, 500);
 canvas.center();
 video = createCapture(VIDEO);
 video.hide();
+
+posenet = ml5.poseNet(video, modelLoaded);
+posenet.on('pose', got_poses);
 }
 
 function draw(){
 image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+   
+ if(results.length > 0){
+   console.log(results);
+   
+   rightWristX = results[0].pose.rightWrist.x;
+   rightWristY = results[0].pose.rightWrist.y;
+   console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+   
+   leftWristX = results[0].pose.leftWrist.x;
+   leftWristY = results[0].pose.leftWrist.y;
+   console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+  }
 }
